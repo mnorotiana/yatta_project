57cc2437225a2200cb9dada7a1dc48fb42b1e168
RATAHIRY MARISON Medrick Mirah <Mirah>
2016-10-28 18:49:27 +0300

    Push model

---
+++ project/api/controllers/Historique_intrusionController.js
@@ -0,0 +1,11 @@
+/**
+ * Historique_intrusionController
+ *
+ * @description :: Server-side logic for managing historique_intrusions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ project/api/controllers/Lien_intrusion_categorieController.js
@@ -0,0 +1,11 @@
+/**
+ * Lien_intrusion_categorieController
+ *
+ * @description :: Server-side logic for managing lien_intrusion_categories
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
